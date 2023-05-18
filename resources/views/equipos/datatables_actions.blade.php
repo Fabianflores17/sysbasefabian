f7b9b4d847e3ee3a4824a74ce8577a2130c7dc1f
Fabian Flores <ffloresl1@miumg.edu.gt>
2023-05-18 08:18:02 -0600

    Actualizacion de botones Equipo

--- resources/views/equipos/datatables_actions.blade.php
+++ resources/views/equipos/datatables_actions.blade.php
@@ -1,17 +1,17 @@
 @can('Ver Equipos')
-    <a href="{{ route('equipos.show', $id) }}" data-toggle="tooltip" title="Ver" class='btn btn-icon btn-flat-secondary rounded-circle'>
+    <a href="{{ route('equipos.show', $id) }}" data-toggle="tooltip" title="Ver" class='btn btn-sm btn-outline-secondary'>
         <i class="fa fa-eye"></i>
     </a>
-@endcan
+    @endcan
 
 @can('Editar Equipos')
-    <a href="{{ route('equipos.edit', $id) }}" data-toggle="tooltip" title="Editar" class='btn btn-icon btn-flat-info rounded-circle'>
+    <a href="{{ route('equipos.edit', $id) }}" data-toggle="tooltip" title="Editar" class='btn btn-sm btn-outline-info'>
         <i class="fa fa-edit"></i>
     </a>
 @endcan
 
 @can('Eliminar Equipos')
-    <a href="#" onclick="deleteItemDt(this)" data-id="{{ $id }}" data-toggle="tooltip" title="Eliminar" class='btn btn-icon btn-flat-danger rounded-circle'>
+    <a href="#" onclick="deleteItemDt(this)" data-id="{{ $id }}" data-toggle="tooltip" title="Eliminar" class='btn btn-sm btn-outline-danger'>
         <i class="fa fa-trash-alt"></i>
     </a>
 
