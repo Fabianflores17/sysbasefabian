a4c52406f484241ef00a596100db67f6c302124d
Fabian Flores <ffloresl1@miumg.edu.gt>
2023-05-18 08:53:26 -0600

    actulizacion fields Equipo

--- resources/views/equipos/fields.blade.php
+++ resources/views/equipos/fields.blade.php
@@ -1,8 +1,18 @@
 <!-- Tipo Id Field -->
-<div class="form-group col-sm-6">
-    {!! Form::label('tipo_id', 'Tipo Id:') !!}
-    {!! Form::number('tipo_id', null, ['class' => 'form-control', 'required']) !!}
-</div>
+<!-- Tipo Equipo Id Field -->
+<div class="form-group col-sm-6" id="root">
+
+    <label for="tipoequipo">Tipo </label>
+
+    <multiselect v-model="tipoequipo" :options="tipoequipos" label="nombre" placeholder="Selecciones uno" >
+
+    </multiselect>
+
+    <input type="hidden" name="tipo_id" id="tipo_id" :value="tipoId">
+
+    <input type="hidden" name="tipo_name" id="tipo_name" :value="tipoName">
+
+  </div>
 
 <!-- Numero Serie Field -->
 <div class="form-group col-sm-6">
@@ -21,3 +31,62 @@
     {!! Form::label('observaciones', 'Observaciones:') !!}
     {!! Form::textarea('observaciones', null, ['class' => 'form-control', 'maxlength' => 65535, 'maxlength' => 65535, 'maxlength' => 65535]) !!}
 </div>
+@push('scripts')
+
+<script>
+
+    $(function () {
+
+        $("input[name='q']").hide();
+
+        });
+
+        const app = new Vue({
+        name:'app',
+        el: '#root',
+        created() {
+
+        this.tipoId;
+        this.tipoName;
+
+        },
+
+
+        data: {
+
+        tipoequipos: @json(\App\Models\TipoEquipo::all() ?? []),
+        tipoequipo: @json(\App\Models\TipoEquipo::whereId(old('tipo_id'))->first() ?? $equipo->tipo ?? null),
+
+
+        },
+
+        computed: {
+
+        tipoId() {
+        if (this.tipoequipo) {
+        return this.tipoequipo.id;
+            }
+        return null;
+
+          },
+
+  tipoName(){
+
+    if (this.tipoequipo){
+  return this.tipoequipo.nombre;
+
+          }
+          return null
+
+        },
+
+}
+
+
+
+
+});
+
+</script>
+
+@endpush
