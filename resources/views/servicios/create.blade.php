3e22912abeff2371e0f83ae0e66d394731600f29
Fabflores17 <gyfabflores@micoopeguayacan.gt>
2024-08-21 16:19:57 -0600

    cambios en servicios

--- resources/views/servicios/create.blade.php
+++ resources/views/servicios/create.blade.php
@@ -10,7 +10,7 @@
         <div class="container-fluid">
             <div class="row mb-2">
                 <div class="col-sm-6">
-                    <h1>Crear Servicio</h1>
+                    <h1>Crear Servicio 2025</h1>
                 </div>
                 <div class="col ">
                     <a class="btn btn-outline-info float-right"
